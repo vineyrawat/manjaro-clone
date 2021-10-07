3378cdfc938a6aa5e82cf43a57fdbb0b38972653
Vinay Rawat <vineyrawat@yahoo.com>
2021-10-07 09:33:22 +0530

    Added manjaro logo in dock and changed bg opacity of dock

--- src/components/main/dock/index.tsx
+++ src/components/main/dock/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { FaUbuntu } from "react-icons/fa";
+import ManjaroLogo from "../../../assets/logos/manjaro.png";
 
 const Dock = () => {
   return (
-    <div className="bg-white dark:bg-black bg-opacity-80 dark:opacity-60 h-12 flex space-x-2 items-center">
-      <FaUbuntu size={30} className="ml-3 cursor-pointer" />
+    <div className="bg-white dark:bg-black bg-opacity-60 dark:opacity-60 h-12 flex space-x-2 items-center">
+      <img
+        src={ManjaroLogo}
+        alt="Manjaro"
+        className="h-full p-2 transition-colors cursor-pointer brightness-50 dark:brightness-100"
+      />
     </div>
   );
 };
